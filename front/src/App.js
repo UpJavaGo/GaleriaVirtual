8c0618aecaa40159dee8cc16b27df57c1e1f40d3
Diego Rey <diegohrey@gmail.com>
2022-10-23 19:21:06 -0500

    clase octubre 13

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Galeria Virtual</h1>
+        <img src="./images/hermoso.jpg" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
