487dea1f38a7f5bd7f6e19822ed07f8916b1869c
Diego Rey <diegohrey@gmail.com>
2022-10-25 16:29:32 -0500

    constants, reducer,s actions

--- front/src/Components/Home.js
+++ front/src/Components/Home.js
@@ -1,20 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import MetaData from './Layout/MetaData'
+import {useDispatch} from 'react-redux'
+import { getProducts } from '../actions/productActions'
 
-export const Home = () => {
-  return (
-    <Fragment>
-        <MetaData>title="La mejor inversion"</MetaData>
-        <h1 id="encabezado_productos">Ultimas Obras</h1>
+export const Home = () => {  
+    const dispatch=useDispatch();
+    useEffect(() => {
+        dispatch(getProducts());
+    }, [dispatch])
+    return (
+        <Fragment>
+            <MetaData>title="La mejor inversion"</MetaData>
+            <h1 id="encabezado_productos">Ultimas Obras</h1>
 
-        <section id="productos" className='container mt-5'>
-            <div className='row'>
-                {/*Producto 1*/}
-                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                    <div className='card p3 rounded'>
-                        <img className='card-img-top mx-auto' src='./images/hermo1.png' alt="hermo1"></img>
-                        <div className='card-body d-flex flex-column'>
-                          <h5 id="titulo_producto"><a href='http://localhost:3000'>Pintura al Oleo</a></h5>
+            <section id="productos" className='container mt-5'>
+                <div className='row'>
+                    {/*Producto 1*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                        <div className='card p3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/hermo1.png' alt="hermo1"></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id="titulo_producto"><a href='http://localhost:3000'>Pintura al Oleo</a></h5>
                           <div className='rating mt-auto'>
                               <div className='rating-outer'>
                                   <div className='rating-inner'></div>
